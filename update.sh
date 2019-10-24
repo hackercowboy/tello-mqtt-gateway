59b032e015e9f30ea7b39abd9c0b3be1bf533ae7
David Uebelacker <david.uebelacker@endress.com>
2019-10-24 21:16:23 +0200

    :hankey: Q:	What's the difference between an Irish wedding and an Irish wake? A:	One less drunk.

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ docker build -t tello-mqtt-gateway .
 docker stop tello-mqtt-gateway >> /dev/null
 docker rm tello-mqtt-gateway >> /dev/null
 
-docker run -d --name --restart always tello-mqtt-gateway tello-mqtt-gateway
+docker run -d --restart always --name tello-mqtt-gateway tello-mqtt-gateway
 
